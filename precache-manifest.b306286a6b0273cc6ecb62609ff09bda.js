a30d1987e84ade859ade3cd182ad1035743bb663
daniel <DanielJamesHewlett@gmail.com>
2020-07-08 14:47:54 +0200

    Updates

---
+++ precache-manifest.b306286a6b0273cc6ecb62609ff09bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42326e4ac86cb9c71d06d742c5f36b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "749d4ee4bd9eb519700b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d9d12d1a1e6bd313aee7",
+    "url": "/static/js/2.311a1a6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.311a1a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749d4ee4bd9eb519700b",
+    "url": "/static/js/main.0cee0fa7.chunk.js"
+  },
+  {
+    "revision": "ddb23d41b025de166703",
+    "url": "/static/js/runtime-main.60ded422.js"
+  }
+]);
